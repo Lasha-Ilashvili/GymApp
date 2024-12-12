9c87eea7e83945495365a225102a2f7bf6950fe0
Lasha-Ilashvili <ilashvililasha@gmail.com>
2024-12-12 14:02:19 +0400

    Added a logger

--- src/main/java/com/internship/gym/data/service/TrainerService.java
+++ src/main/java/com/internship/gym/data/service/TrainerService.java
@@ -3,11 +3,14 @@ package com.internship.gym.data.service;
 import com.internship.gym.data.dao.TrainerDao;
 import com.internship.gym.data.model.user.Trainer;
 import com.internship.gym.utils.Generator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainerService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TrainerService.class);
     private final TrainerDao trainerDao;
 
     public TrainerService(final TrainerDao trainerDao) {
@@ -21,14 +24,27 @@ public class TrainerService {
         trainer.setUsername(username);
         trainer.setPassword(password);
 
+        logger.info("Trainer created successfully: id={}, username={}", trainer.getTrainerId(), trainer.getUsername());
+
         return trainerDao.addTrainer(trainer);
     }
 
     public Trainer updateTrainer(final Trainer updatedTrainer) {
+        logger.info("Trainer updated successfully: id={}", updatedTrainer.getTrainerId());
+
         return trainerDao.addTrainer(updatedTrainer);
     }
 
     public Trainer selectTrainer(final long trainerId) {
-        return trainerDao.getTrainer(trainerId);
+        Trainer selectedTrainer = trainerDao.getTrainer(trainerId);
+
+        if (selectedTrainer != null) {
+            logger.info("Trainer found: id={}, firstName={}, lastName={}", selectedTrainer.getTrainerId(),
+                    selectedTrainer.getFirstName(), selectedTrainer.getLastName());
+        } else {
+            logger.info("Trainer not found: id={}", trainerId);
+        }
+
+        return selectedTrainer;
     }
 }
--- src/main/java/com/internship/gym/data/service/TrainingService.java
+++ src/main/java/com/internship/gym/data/service/TrainingService.java
@@ -2,11 +2,14 @@ package com.internship.gym.data.service;
 
 import com.internship.gym.data.dao.TrainingDao;
 import com.internship.gym.data.model.training.Training;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TrainingService.class);
     private final TrainingDao trainingDao;
 
     public TrainingService(final TrainingDao trainingDao) {
@@ -14,10 +17,21 @@ public class TrainingService {
     }
 
     public Training createTraining(final Training training) {
+        logger.info("Training created successfully: name={}, id={}", training.getTrainingName(), training.getTrainerId());
+
         return trainingDao.addTraining(training);
     }
 
     public Training selectTraining(final long trainerId, final long traineeId) {
-        return trainingDao.getTraining(trainerId, traineeId);
+        Training selectedTraining = trainingDao.getTraining(trainerId, traineeId);
+
+        if (selectedTraining != null) {
+            logger.info("Training found: name={}, date={}, duration={}", selectedTraining.getTrainingName(),
+                    selectedTraining.getTrainingDate(), selectedTraining.getTrainingDuration());
+        } else {
+            logger.info("Training not found: trainerId={}, traineeId={}", trainerId, traineeId);
+        }
+
+        return selectedTraining;
     }
 }
